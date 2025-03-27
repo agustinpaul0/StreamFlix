9c2ec72b438977ce98b0885ef919986de24bba09
Agustin Paul <a.agustinpaul@gmail.com>
2025-03-27 04:11:16 -0300

    Finish problems with SuspenseQuery, movies and series screens must be reviewed in the future

--- src/screens/SeriesScreen.tsx
+++ src/screens/SeriesScreen.tsx
@@ -6,6 +6,7 @@ import SplashScreen from "./SplashScreen";
 const SeriesScreen = () => {
   return (
     <>
+      {/*Don't know why I had to sorround it with a Suspense in order to work properly*/}
       <Suspense fallback={<SplashScreen />}>
         <h3 className="text-2xl font-medium pl-4 pt-4">Our Recommendation</h3>
         <PopularSeries />
