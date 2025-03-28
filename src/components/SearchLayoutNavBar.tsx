44d9b89367f5ea5131a546fae7c666bcbafed83c
Agustin Paul <a.agustinpaul@gmail.com>
2025-03-28 03:30:59 -0300

    Improve search UX and finish search functionality

--- src/components/SearchLayoutNavBar.tsx
+++ src/components/SearchLayoutNavBar.tsx
@@ -1,4 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { 
+  useState, 
+  useEffect 
+} from "react";
 import search from "../assets/img/search-icon.svg";
 import logo from "../assets/img/logo.svg";
 import useRedirect from "../hooks/useRedirect";
@@ -8,31 +11,46 @@ const SearchLayoutNavBar = () => {
   const [searchMedia, setSearchMedia] = useState("");
   const handleRedirect = useRedirect();
   const location = useLocation();
-  const inputRef = useRef<HTMLInputElement>(null);
-  const SEARCH_MEDIA_SCREEN = `/streamflix/search/media?user_search=${searchMedia}`;
+
+  const SEARCH_MEDIA_SCREEN_URL = `/streamflix/search/media?user_search=${searchMedia}`;
+  const HOME_SCREEN_URL = `/streamflix/search/home`;
 
   useEffect(() => {
-    setSearchMedia("");
-    inputRef.current?.blur(); // To remove the autofocus after searching
-  },[location]);
+    const queryParams = new URLSearchParams(location.search);
+    const userSearchParam = queryParams.get('user_search');
+    
+    if (userSearchParam) {
+      setSearchMedia(userSearchParam);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(location.pathname === HOME_SCREEN_URL) {
+      setSearchMedia("");
+    }
+  }, [location]);
+
+  useEffect(() => {
+    (searchMedia.length > 0)
+      ? handleRedirect(SEARCH_MEDIA_SCREEN_URL)
+      : handleRedirect(HOME_SCREEN_URL);
+  }, [searchMedia]);
 
   return (
     <nav className="fixed top-0 left-0 w-full flex p-4 gap-4 bg-[#080808] z-40 h-21">
       <img src={logo} alt="App Logo" />
       <div className="relative flex-grow">
         <input
-          ref={inputRef}
           type="text"
           placeholder="Type a title, genre, or actor..."
           value={searchMedia}
           onChange={(event) => setSearchMedia(event.target.value)}
-          onKeyDown={(event) => (event.key === "Enter") ? handleRedirect(SEARCH_MEDIA_SCREEN) : null}
           spellCheck={false}
           className="rounded-full bg-[#1E1E1E] text-[#827E7E] pl-3 w-full h-full border-2 flex-grow border-[#1E1E1E] focus:outline-none focus:border-[#1E1E1E]"
         />
         <button
           type="button"
-          onClick={() => handleRedirect(SEARCH_MEDIA_SCREEN)}
+          onClick={() => handleRedirect(SEARCH_MEDIA_SCREEN_URL)}
           className="absolute right-0 top-1/2 transform -translate-y-1/2 p-3 bg-[#1E1E1E] text-white rounded-full focus:outline-none cursor-pointer"
         >
           <img src={search} alt="Search Icon" className="w-5 h-5" />
