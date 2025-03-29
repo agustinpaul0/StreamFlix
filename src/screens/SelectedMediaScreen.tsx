2c5b05e0d42e26fc0c320923963a1d30efa18f8f
Agustin Paul <a.agustinpaul@gmail.com>
2025-03-29 00:00:15 -0300

    Add a Suspense component to correctly manage the loading state while navigating to SelectedMediaScreen component route

--- src/screens/SelectedMediaScreen.tsx
+++ src/screens/SelectedMediaScreen.tsx
@@ -2,14 +2,19 @@ import { useOutletContext } from "react-router-dom";
 import MediaBanner from "../components/MediaBanner";
 import MediaDescription from "../components/MediaDescription";
 import Media from "../types/Media";
+import SplashScreen from "./SplashScreen";
+import { Suspense } from "react";
 
 const SelectedMediaScreen = () => {
   const { selectedMedia } = useOutletContext<{ selectedMedia: Media}>();
 
   return (
     <>
-      <MediaBanner media={selectedMedia} />
-      <MediaDescription media={selectedMedia} />
+      {/*Don't know why I had to sorround it with a Suspense in order to work properly*/}
+      <Suspense fallback={<SplashScreen />}>
+        <MediaBanner media={selectedMedia} />
+        <MediaDescription media={selectedMedia} />
+      </Suspense>
     </>
   );
 };
